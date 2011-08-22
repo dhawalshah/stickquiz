36f6ce2cea2d50a2583c5979e562e543956dfbde
Dhawal <dhawaljshah@gmail.com>
2011-08-22 23:47:26 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stickquiz::Application.config.secret_token = '896c43f99bc9f1949d387ae689a7b8c99d3bff13a903d0ee37a34011a17e6a54ff621e07003de19b5d88b242840423cdd5642e349647ffd2ddf39fe14ff9902b'
