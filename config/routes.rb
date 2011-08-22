d21b6129811a4fb22993eb12325871d4ce6fa7fb
Dhawal Shah <dhawaljshah@gmail.com>
2011-08-23 00:40:03 +0800

    remove crap routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Stickquiz::Application.routes.draw do
-  resources :quizzes
+  resources :quizzes, :only => :index
+  match '/result' => 'quizzes#result'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +51,7 @@ Stickquiz::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "quizzes#index"
 
   # See how all your routes lay out with "rake routes"
 
