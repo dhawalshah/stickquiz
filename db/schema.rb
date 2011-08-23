dc100fb1f284023e70123b35255642c57a66030d
Dhawal Shah <dhawaljshah@gmail.com>
2011-08-24 00:31:00 +0800

    fixtures and option model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110822161340) do
+ActiveRecord::Schema.define(:version => 20110823162444) do
+
+  create_table "options", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", :force => true do |t|
     t.integer  "answer"
